77bbb43bd5b66666ab0c6d534fb242047b4649ec
nadunindunil <nadun1indunil@gmail.com>
2016-09-28 18:14:57 +0530

    init with navbar, users and group created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'groups', component: GroupComponent },
+  { path: 'users', component: UsersComponent },
+  { path: '', component: HomeComponent }
+  // ,
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    UsersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
